77f60eed3fa5c28239b299f28d7b7d5297bbefd5
waniaali947 <164549121+waniaali947@users.noreply.github.com>
2024-10-13 09:16:12 +0500

    Add files via upload

---
+++ class 6/src/conditions.ts
@@ -0,0 +1,88 @@
+let classDay: string = "wednesday";
+
+if (classDay === "wednesday") {
+  console.log("Go to class");
+} else {
+  console.log("Do nothing sleep and have a nice day");
+}
+
+
+let firstEid :number = 5000;
+let secondEid : number = 2000;
+if(firstEid === secondEid) {
+
+    console.log(5000);
+    } else if (secondEid <= firstEid) {
+        console.log("Not give me a eidi")
+    
+}else {
+    console.log(secondEid)
+}
+
+let homeFood : string = "biryani";
+let moneyIHave : number = 100;
+let pizza : number = 400;
+
+if (homeFood === "biryani" && moneyIHave === 300){ //&& logic operators
+    console.log("Go out and buy pizza");
+
+} else if(moneyIHave >= pizza) {
+    console.log("Buy pizza");
+
+}else {
+    console.log("Eat biryani");
+}
+
+
+let monthsNumber = 8
+switch (monthsNumber) {
+    case 1 :
+        console.log("January"); 
+        break ;
+        case 2 :
+            console.log("Feburary");
+            break ;
+            case 3 :
+                console.log("March");
+                break ;
+                case 4 :
+                    console.log("April");
+                    break ;
+                    case 5 :
+                        console.log("May");
+                        break ;
+                        case 6 :
+                            console.log("June");
+                            break ;
+                            case 7 :
+                                console.log("July");
+                                break ;
+                                        case 8 :
+                                         console.log("August");
+                                         break ;
+                                              case 9 :
+                                              console.log("September");
+                                              break ;
+                                                     case 10 :
+                                                      console.log("october");
+                                                      break ;
+                                                              case 11 :
+                                                               console.log("November");
+                                                               break ;
+                                                                       case 12 :
+                                                                          console.log("December")
+
+
+}
+
+let isMyBirthday : boolean = true;
+let amIHappy : boolean = false;
+// logic and
+if(isMyBirthday && amIHappy) {
+    console.log("Happy Birthday");
+}
+
+// logic or
+if(isMyBirthday || amIHappy) {
+    console.log("Happy birthday am not Happy");
+};
